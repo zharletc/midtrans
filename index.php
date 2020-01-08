5b2fa5486176466f82525dcba4b324159d064d5a
Azhar Ogi <azharogi@gmail.com>
2020-01-08 16:06:38 +0700

    First commit

--- index.php
+++ index.php
@@ -9,15 +9,15 @@ $api_url = $is_production ?
   'https://app.sandbox.midtrans.com/snap/v1/transactions';
 
 
-if( !strpos($_SERVER['REQUEST_URI'], '/charge') ) {
-  http_response_code(404); 
-  echo "wrong path, make sure it's `/charge`"; exit();
-}
+// if( !strpos($_SERVER['REQUEST_URI'], '/charge') ) {
+//   http_response_code(404); 
+//   echo "wrong path, make sure it's `/charge`"; exit();
+// }
 
-if( $_SERVER['REQUEST_METHOD'] !== 'POST'){
-  http_response_code(404);
-  echo "Page not found or wrong HTTP request method is used"; exit();
-}
+// if( $_SERVER['REQUEST_METHOD'] !== 'POST'){
+//   http_response_code(404);
+//   echo "Page not found or wrong HTTP request method is used"; exit();
+// }
 
 $request_body = file_get_contents('php://input');
 header('Content-Type: application/json');
